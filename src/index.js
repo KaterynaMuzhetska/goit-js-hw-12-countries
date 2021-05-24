45d2fa705eba38a376ff9412fc7b5e61d4f15d9e
KaterynaMuzhetska <74104436+KaterynaMuzhetska@users.noreply.github.com>
2021-05-24 22:38:18 +0300

    hw

---
+++ src/index.js
@@ -0,0 +1,9 @@
+  
+import templateMenuCard from './templates/menu-template.hbs';
+import dataMenuCard from './js/menu.json';
+import { randerPage } from './js/script-rander-page.js';
+import { changeThemsByClickAndRelodePage } from './js/script-change-thema.js';
+import './sass/main.scss';
+
+randerPage(dataMenuCard, templateMenuCard);
+changeThemsByClickAndRelodePage();
