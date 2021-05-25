edfe850a65f5750ec2e3b3de2017aced0915263a
KaterynaMuzhetska <74104436+KaterynaMuzhetska@users.noreply.github.com>
2021-05-25 23:51:17 +0300

    HW-12

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
-  
-import templateMenuCard from './templates/menu-template.hbs';
-import dataMenuCard from './js/menu.json';
-import { randerPage } from './js/script-rander-page.js';
-import { changeThemsByClickAndRelodePage } from './js/script-change-thema.js';
-import './sass/main.scss';
+import countries from './fetchCountries';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
+import template from './templates/countryTemplate.hbs';
+import countriesListTemplate from './templates/countriesListTemplate.hbs';
+var debounce = require('lodash.debounce');
 
-randerPage(dataMenuCard, templateMenuCard);
-changeThemsByClickAndRelodePage();
+
+
+const cardContainer = document.querySelector('.js-card-container');
+
+const input = document.querySelector('input');
+
+input.addEventListener('input', debounce(searchCounntries, 500));
+
+function searchCounntries(e) {
+    countries(e.target.value).then(countries => {
+        const countriesLength = countries.length;
+        if (countriesLength === 1) {
+            cardContainer.innerHTML = template(countries[0]);
+            return;
+        } else if (countriesLength <= 10) {
+            cardContainer.innerHTML = countriesListTemplate(countries);
+            return;
+        }
+        else {
+            onFetchError();
+        }
+     }).catch(error => {
+        console.log(error);
+    });
+}
+
+
+
+function onFetchError() {
+    error({
+      text: 'Too many matches found. Please enter a more spesific query!',
+      delay: '2000',
+      maxTextHeight: null,
+    });
+  }
